d96b86ab19dd53cbdd530e94a49575f78bcd30b0
Kim myung hyun <81784735+Kimh0411@users.noreply.github.com>
2022-09-05 10:55:10 +0900

    [Bronze V] Title: 합, Time: 60 ms, Memory: 5496 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cs
@@ -0,0 +1,10 @@
+ int n = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = 0; i <= n; i++)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine(sum);
