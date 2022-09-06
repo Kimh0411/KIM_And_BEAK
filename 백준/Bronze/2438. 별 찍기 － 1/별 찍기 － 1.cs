4282f9f468eebb86e90567a6ccab2fedb41aa13f
Kim myung hyun <81784735+Kimh0411@users.noreply.github.com>
2022-09-06 09:40:06 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 288 ms, Memory: 6580 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cs
@@ -0,0 +1,10 @@
+int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j <=i ; j++)
+                {
+                    Console.Write("*"); // 두번째에서 별찍기
+                }
+                Console.Write("\n"); //첫번째 포문 에서줄바꿈
+            }
